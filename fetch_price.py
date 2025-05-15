de64e4bf0dff221157e206ddb0b4bb1ad6973e7d
MirzaFaris12 <156393988+MirzaFaris12@users.noreply.github.com>
2025-05-15 00:04:27 -0400

    Update fetch_price.py

--- fetch_price.py
+++ fetch_price.py
@@ -1,15 +1,10 @@
-import requests, os
+import yfinance as yf
 import pandas as pd
-import streamlit as st
 
 def fetch_price(ticker):
-    key = st.secrets["ALPHAVANTAGE_API_KEY"]
-    url = url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=compact&apikey={key}"
-    r = requests.get(url)
-    data = r.json().get("Time Series (Daily)", {})
-    df = pd.DataFrame.from_dict(data, orient="index")
-    df.index = pd.to_datetime(df.index)
-    df = df.rename(columns={"4. close": "Close"})
-    df["Close"] = df["Close"].astype(float)
-    return df.sort_index()
+    data = yf.download(ticker, period="1mo", interval="1d")
+    if data.empty:
+        return None
+    return data[["Close"]]
+
 
